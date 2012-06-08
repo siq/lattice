288d2b3d0145863b9762977708e9884240d6fb49
Aaron Colichia <acolichia@storediq.com>
2012-06-07 23:56:38 -0500

    Clean up tarball task

--- lattice/tasks/tgz.py
+++ lattice/tasks/tgz.py
@@ -1,10 +1,4 @@
-from bake import *
-from bake.filesystem import Collation
-from scheme import *
-
-from lattice.support.repository import Repository
-from lattice.support.specification import Specification
-from lattice.util import uniqpath
+from scheme import Task, Map, Sequence, Text
 
 class BuildTgz(Task):
     name = 'lattice.tgz.build'
@@ -12,26 +6,13 @@ class BuildTgz(Task):
     parameters = {
         'environ': Map(Text(nonnull=True), description='environment for the build'),
         'name': Text(description='name of the component to build', nonempty=True),
-        'specification': Field(hidden=True),
         'filepaths': Sequence(item=Text(nonnull=True),nonnull=True),
+        'version': Text(description='version of the component to package', nonempty=True)
     }
 
     def run(self, runtime):
-        component = self['specification']
-        if not component:
-            specification = Specification().read()
-            component = specification.get_component(self['name'])
-            if not component:
-                raise TaskError('unknown component')
-
-        if 'builds' not in component:
-            raise TaskError('component has no builds')
-        
         environ = self['environ']
-        if environ is None:
-            environ = {}
-
-        self.pkgname = '%s-%s.tar.bz2' % (self['name'], component['version'])
+        self.pkgname = '%s-%s.tar.bz2' % (self['name'], self['version'])
         runtime.chdir(runtime.curdir[:-len(self['name'])])
         self.filepaths = []
         [self.filepaths.append(ofilepath[len(str(runtime.curdir))+1:]) for \
@@ -44,3 +25,4 @@ class BuildTgz(Task):
         shellargs = ['pax', '-wjvf', str(self.distpath / self.pkgname), '-s', ',^,\/,']
         shellargs.extend(self.filepaths)
         runtime.shell(shellargs, environ=environ, merge_output=True)
+
