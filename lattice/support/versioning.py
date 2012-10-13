1feb6d94fc055e6457d6a55c20b17d55a6c324b7
Jordan McCoy <mccoy.jordan@gmail.com>
2012-10-12 21:42:33 -0500

    better versioning algorithm

--- lattice/support/versioning.py
+++ lattice/support/versioning.py
@@ -1,38 +1,44 @@
 import re
 from functools import total_ordering
 
-VERSION_EXPR = re.compile(r'^([0-9]+)[.]([0-9]+)[.]([0-9]+)(?:([a-z]{1,2})([0-9]+))?(?:[+]([0-9]+))?$')
+VERSION_EXPR = re.compile(
+    r'^([0-9]+(?:[.][0-9]+)+)'
+    r'(?:([a-z]{1,2})([0-9]+))?'
+    r'(?:[+]([1-9][0-9]*))?$')
+
+PRERELEASE_TAGS = ['a', 'b', 'rc']
+POSTRELEASE_TAGS = ['p']
+SYMBOLIC_VERSIONS = ['HEAD']
+TAGS = ['a', 'b', 'rc', 'p']
 
 @total_ordering
 class VersionToken(object):
-    SYMBOLS = ['HEAD']
-
     def __init__(self, version):
         if isinstance(version, VersionToken):
-            version = version.version
+            self.version = version.version
+            self.symbolic = version.symbolic
+            self.tokens = version.tokens
+            self.tag = version.tag
+            self.epoch = version.epoch
+            return
 
         self.version = version
-        if version in self.SYMBOLS:
+        self.symbolic = False
+        self.tokens = None
+        self.tag = None
+        self.epoch = None
+
+        if version in SYMBOLIC_VERSIONS:
             self.symbolic = True
         else:
-            match = VERSION_EXPR.match(version)
-            if match:
-                self.symbolic = False
-                self.tokens = (
-                    int(match.group(1)),
-                    int(match.group(2)),
-                    int(match.group(3)) if match.group(3) is not None else None,
-                    match.group(4),
-                    int(match.group(5)) if match.group(5) is not None else None)
-            else:
-                raise ValueError(version)
+            self._parse_version(version)
 
     def __eq__(self, other):
         if not isinstance(other, VersionToken):
             try:
                 other = VersionToken(other)
             except ValueError:
-                return NotImplementedError()
+                return NotImplemented
 
         return other.version == self.version
 
@@ -44,17 +50,61 @@ class VersionToken(object):
             try:
                 other = VersionToken(other)
             except ValueError:
-                raise NotImplementedError()
+                return NotImplemented
 
         if self.symbolic and other.symbolic:
-            return self.SYMBOLS.index(self.version) < self.SYMBOLS.index(other.version)
-        elif not self.symbolic and not other.symbolic:
-            return self.tokens < other.tokens
-        else:
+            return SYMBOLIC_VERSIONS.index(self.version) < SYMBOLIC_VERSIONS.index(other.version)
+        elif self.symbolic or other.symbolic:
             return not self.symbolic
+        else:
+            return self._compare_version(other)
 
     def __repr__(self):
-        return 'VersionToken(%r)' % self.version
+        if self.symbolic:
+            return 'VersionToken(%r)' % self.version
+        else:
+            return 'VersionToken(%r, %r, %r)' % (self.tokens, self.tag, self.epoch)
 
     def __str__(self):
         return self.version
+
+    def _compare_version(self, other):
+        if self.tokens != other.tokens:
+            return self.tokens < other.tokens
+
+        if self.tag:
+            if other.tag:
+                if self.tag[0] == other.tag[0]:
+                    if self.tag[1] != other.tag[1]:
+                        return self.tag[1] < other.tag[1]
+                else:
+                    return TAGS.index(self.tag[0]) < TAGS.index(other.tag[0])
+            else:
+                return self.tag[0] in PRERELEASE_TAGS
+        elif other.tag:
+            return other.tag[0] in POSTRELEASE_TAGS
+
+        if self.epoch and other.epoch:
+            return self.epoch < other.epoch
+        elif self.epoch or other.epoch:
+            return self.epoch is None
+        else:
+            return False
+
+    def _parse_version(self, version):
+        match = VERSION_EXPR.match(version)
+        if match:
+            self.tokens = tuple(int(value) for value in match.group(1).split('.'))
+        else:
+            raise ValueError(version)
+        
+        tag = match.group(2)
+        if tag is not None:
+            if tag in PRERELEASE_TAGS or tag in POSTRELEASE_TAGS:
+                self.tag = (tag, int(match.group(3)))
+            else:
+                raise ValueError(version)
+
+        epoch = match.group(4)
+        if epoch is not None:
+            self.epoch = int(epoch)
